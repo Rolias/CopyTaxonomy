0d2bcabbf07baf784f55bf3a1122e9f868ddc402
Tod Gentille <tod-gentille@Tods-Gray-Ghost.local>
2018-09-27 17:00:36 -0700

    reset column selector values

--- ActiveSheet.js.js
+++ ActiveSheet.js.js
@@ -1,15 +1,15 @@
 /* exported onEdit */
 function onEdit() {
-  
+
   const ss = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   var cell = ss.getCurrentCell();
   var col = cell.getColumn();
   var row = cell.getRow();
-  
+
   const SUPER_DOMAIN_COL = 1;
   const PRIMARY_DOMAIN_COL = 2;
   const SETTINGS_ROW = 2;
-  
+
   if ((col === SUPER_DOMAIN_COL) && (row === SETTINGS_ROW)) {
     ss.getRange("PrimarySubSettings").setValue("");
   } else if (col == PRIMARY_DOMAIN_COL && row == SETTINGS_ROW) {
@@ -17,11 +17,22 @@ function onEdit() {
   }
 }
 
-
-function resetColumnSelection(){
+/* exported resetColumnSelection */
+function resetColumnSelection() {
   const COLUMN_SELECTOR_AREA = "ColumnSelectors";
   const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const selectColArea = ss.getRangeByName("COLUMN_SELECTOR_AREA");
- 
- 
+  const selectColArea = ss.getRangeByName(COLUMN_SELECTOR_AREA);
+
+  var col;
+  for (col = 1; col < 6; col += 2) {
+    selectColArea[col].forEach(function (element, index) {
+      element.setValue = false;
+      if (col < 2 && index < 8) {
+        element.setValue(true);
+      } else {
+        element.setValue(false);
+      }
+    });
+  }
+
 }
