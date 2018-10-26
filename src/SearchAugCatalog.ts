80e90b5ef398b07a4ab652276d8fe48531e89c4c
Tod Gentille <tod-gentille@Tods-Gray-Ghost.lan>
2018-10-25 18:43:24 -0700

    add export back

--- src/SearchAugCatalog.ts
+++ src/SearchAugCatalog.ts
@@ -4,7 +4,7 @@ import utils from "./Utils";
 const AUG_CATALOG_ID = "1U5Pv_Bljnl1hCn9yJetn8adnXmzi8JQe_RAPdoLcxOY";
 const TAB_NAME = "CourseData";
 
-function importAugmentedData(): void {
+export function importAugmentedData(): void {
   const fullData = getAugmentedCatalogData();
   const sheet = SheetCopier.getActiveSheetByName(utils.AUG_CATALOG_IMPORT_SHEET);
   SheetCopier.copyDataToSheet(fullData, sheet);
